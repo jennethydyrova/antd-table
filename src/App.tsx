c4a9d2ce4cc7cf2d6b37869a885658de18d92d26
Anonymous <>
2021-09-02 12:45:16 +0300

    add basic page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "antd/dist/antd.css";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Layout } from "antd";
+import Bills from "./components/Bills";
+import PaymentMethods from "./components/PaymentMethods";
+import Services from "./components/Services";
+import Settings from "./components/Settings";
 
 function App() {
+  const { Footer, Content } = Layout;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Layout style={{ minHeight: "100vh" }}>
+          <Sidebar />
+          <Layout>
+            <Content style={{ margin: "24px 16px 0" }}>
+              <div className="site-layout-background">
+                <Route exact path="/" component={Bills} />
+                <Route
+                  exact
+                  path="/payment-methods"
+                  component={PaymentMethods}
+                />
+                <Route exact path="/services" component={Services} />
+                <Route exact path="/settings" component={Settings} />
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>Foo</Footer>
+          </Layout>
+        </Layout>
+      </Router>
     </div>
   );
 }
